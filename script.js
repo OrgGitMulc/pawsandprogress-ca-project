85d6eb7a205d747ae8e1069f126ac77e63173b88
GitMulC <cmulvo@gmail.com>
2024-04-08 20:56:07 +0000

    Resolve meging conflicts

--- script.js
+++ script.js
@@ -103,56 +103,6 @@ function game_logic(q_num,guess_num){
     ans_area.classList.add('hidden');
 }
 
-/**
- * Global Variables
- */
-
-let start_btn = document.getElementById('start_btn');
-let ans_area = document.getElementById('ans_area');
-
-//Imgs
-let pug = document.getElementById('pug');
-let dasc = document.getElementById('dasc');
-let jack = document.getElementById('jack');
-
-var guess = 0;
-
-/**
- * Event Listeners
- */
-
-start_btn.addEventListener('click', function () {
-    console.log('Start Game!!!');
-    // start_btn.classList.add('hidden');
-    var question = start_game();
-    console.log(question);
-    ans_area.classList.remove('hidden');
-
-});
-
-function start_game(){
-    var q = Math.floor(Math.random() * 3);
-    if (q == 0){
-        pug.classList.remove('hidden');
-        console.log('Pug Question', q);
-    } else if (q == 1){
-        dasc.classList.remove('hidden');
-        console.log('Daschund Question', q);
-    } else if (q == 2){
-        jack.classList.remove('hidden');
-        console.log('Jack Russell Question', q);
-    } else {
-        console.log('Error!')
-    }
-
-    return q;
-}
-
-function game_logic(q_num,guess_num){
-    q_num = question;
-    guess_num = guess;
-}
-
 //About Us page
 function setNewImageDogs()
       {document.getElementById("happydogs").src="../media/images/happydogs_speech.jpg";}
