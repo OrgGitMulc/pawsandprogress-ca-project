88473d397e18ea635bcbba17d42957ee678f38c6
GitMulC <cmulvo@gmail.com>
2024-04-14 16:44:08 +0000

    create review.js for review form validation

---
+++ review.js
@@ -0,0 +1,17 @@
+//form validation for review form submission
+let form = document.getElementById('revForm');
+let formName = document.getElementById('name');
+let email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    if (formName.value === '' || formName.value == null){
+        alert('Please enter a name!');
+        e.preventDefault();
+    }
+
+    if (email.value === '' || email.value == null){
+        alert('Please enter a email address!');
+        e.preventDefault();
+    }
+
+});
